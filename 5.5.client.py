29dd02bdcbb2e0bb837136fa251ef533daf771ec
ecstahr <adraazlin@gmail.com>
2022-06-26 02:48:30 +0800

    lab 5.5 client

---
+++ 5.5.client.py
@@ -0,0 +1,27 @@
+import socket
+import os
+import sys
+
+#host server ip 
+ip = "192.168.56.110"
+
+#socket creating
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+PORT = 8888
+
+s.connect((ip, PORT))
+
+sendFile = input("\n[?] Enter name of file to send: ")
+file = open(sendFile, "rb")
+sendData = file.read(1024)
+
+s.send(sendFile.encode("utf-8"))
+
+while sendData:
+	print("[+]Message received!\n", s.recv(1024).decode("utf-8"))
+	print("File name: " + sendFile)
+	s.send(sendData)
+	sendData = file.read(1024)
+
+#close socket
+s.close()
